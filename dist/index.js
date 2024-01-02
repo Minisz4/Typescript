8bd13bad8ce0b0ad33f85770c993469a3132b406
Minisz4 <jasmina.skandrani@gmail.com>
2024-01-02 16:50:25 +0100

    x

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,46 @@
-let books;
-let book2;
-let book3;
-let book4;
-let book5;
-let book6;
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function fetchAPI(url) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch(url);
+            if (!response.ok) {
+                throw new Error(`Failed to fetch data. Status: ${response.status}`);
+            }
+            const data = yield response.json();
+            return data;
+        }
+        catch (error) {
+            console.error("Error fetching data:", error);
+            throw error;
+        }
+    });
+}
+function printAPI(data) {
+    let booksContainer = document.getElementById("books");
+    if (booksContainer) {
+        data.forEach((book) => {
+            const bookDiv = document.createElement("div");
+            bookDiv.className = "book";
+            bookDiv.style.backgroundColor = book.color;
+            const titleHeader = document.createElement("h2");
+            titleHeader.textContent = book.title;
+            bookDiv.appendChild(titleHeader);
+            const authorHeader = document.createElement("h3");
+            authorHeader.textContent = book.author;
+            bookDiv.appendChild(authorHeader);
+            booksContainer.appendChild(bookDiv);
+        });
+    }
+}
+const apiURL = "https://my-json-server.typicode.com/zocom-christoffer-wallenberg/books-api/books";
+fetchAPI(apiURL).then((data) => {
+    printAPI(data);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,53 @@
-let books: string;
-let book2: string;
-let book3: string;
-let book4: string;
-let book5: string;
-let book6: string;
+interface Book {
+  audience: string;
+  author: string;
+  color: string;
+  id: number;
+  pages: number | null;
+  plot: string;
+  publisher: string;
+  title: string;
+  year: number;
+}
+
+async function fetchAPI(url: string) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data. Status: ${response.status}`);
+    }
+    const data: Book[] = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+function printAPI(data: Book[]) {
+  let booksContainer: HTMLElement | null = document.getElementById("books");
+
+  if (booksContainer) {
+    data.forEach((book) => {
+      const bookDiv = document.createElement("div");
+      bookDiv.className = "book";
+      bookDiv.style.backgroundColor = book.color;
+
+      const titleHeader = document.createElement("h2");
+      titleHeader.textContent = book.title;
+      bookDiv.appendChild(titleHeader);
+
+      const authorHeader = document.createElement("h3");
+      authorHeader.textContent = book.author;
+      bookDiv.appendChild(authorHeader);
+
+      booksContainer.appendChild(bookDiv);
+    });
+  }
+}
+
+const apiURL: string =
+  "https://my-json-server.typicode.com/zocom-christoffer-wallenberg/books-api/books";
+
+fetchAPI(apiURL).then((data: Book[]) => {
+  printAPI(data);
+});
