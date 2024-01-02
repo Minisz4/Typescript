3d9e3f1f25e11be22f9380ac340dbcc65dd6eff6
Minisz4 <jasmina.skandrani@gmail.com>
2024-01-02 10:50:02 +0100

    x

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+let books;
+let book2;
+let book3;
+let book4;
+let book5;
+let book6;
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let books: string;
+let book2: string;
+let book3: string;
+let book4: string;
+let book5: string;
+let book6: string;
